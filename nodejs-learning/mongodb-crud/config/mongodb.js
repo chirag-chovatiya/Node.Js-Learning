89aedecf6542204ca191b02ccef5055cdd25a848
Chirag Chovatiya <chiragchovatiya92@gmail.com>
2023-08-17 17:48:34 +0530

    one line add

--- nodejs-learning/mongodb-crud/config/mongodb.js
+++ nodejs-learning/mongodb-crud/config/mongodb.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-  .connect(`mongodb://${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}`)
+  .connect(
+    `mongodb://${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}`
+  )
   .then(() => {
     console.log("Connected to Mongodb database");
   })

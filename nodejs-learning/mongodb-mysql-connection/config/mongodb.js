14ff9280adf8bfb41a261ce70ba5c981d13d919a
Chirag Chovatiya <chiragchovatiya92@gmail.com>
2023-08-26 11:36:08 +0530

    one line add

--- nodejs-learning/mongodb-mysql-connection/config/mongodb.js
+++ nodejs-learning/mongodb-mysql-connection/config/mongodb.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-  .connect(`mongodb://${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}`)
+  .connect(
+    `mongodb://${process.env.MONGO_SERVER}/${process.env.MONGO_DATABASE}`
+  )
   .then(() => {
     console.log("Connected to Mongodb database");
   })
   .catch((err) => {
     console.error("Database connection failed");
   });
-
-  

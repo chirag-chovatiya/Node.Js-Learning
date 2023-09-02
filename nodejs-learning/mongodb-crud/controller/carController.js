3db36dad75d49393e1ced2bb15b66b2221076037
Chirag Chovatiya <chiragchovatiya92@gmail.com>
2023-09-02 17:23:12 +0530

    one line add

--- nodejs-learning/mongodb-crud/controller/carController.js
+++ nodejs-learning/mongodb-crud/controller/carController.js
@@ -2,29 +2,25 @@ const User = require("../models/userModel");
 const httpStatus = require("http-status");
 const Joi = require("joi");
 
-
-
 const carSpecification = async (req, res) => {
-    try {
-      const userId  = req.userId; 
-      const carData = req.body;
-        
-      const savedCar = await User.createCar(userId, carData);
-      console.log("firstffffffffffffffffffffff",savedCar)
-      return res.status(httpStatus.OK).json({
-        statusCode: httpStatus.OK,
-        message: "Data create successfully",
-        savedUser: savedCar,
-      });
-    } catch (error) {
-      console.error(error);
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        message: "Unable to create data",
-      });
-    }
-  };
+  try {
+    const userId = req.userId;
+    const carData = req.body;
 
+    const savedCar = await User.createCar(userId, carData);
+    console.log("firstffffffffffffffffffffff", savedCar);
+    return res.status(httpStatus.OK).json({
+      statusCode: httpStatus.OK,
+      message: "Data create successfully",
+      savedUser: savedCar,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: "Unable to create data",
+    });
+  }
+};
 
-
-  module.exports = {carSpecification}
+module.exports = { carSpecification };
